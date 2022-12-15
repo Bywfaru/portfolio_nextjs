4fe1318436c80c9738d8baa82ddcb5984370bd50
William He <contact@williamhe.dev>
2022-12-14 19:18:01 -0800

    Fix import

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { Footer } from "@/components";
-import { Analytics } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
