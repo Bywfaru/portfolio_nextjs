5c312e28747c2b019993125aee5a48e0bec0d7f2
William He <contact@williamhe.dev>
2023-01-26 22:05:04 -0800

    WIP

--- pages/projects/tekken7/index.jsx
+++ pages/projects/tekken7/index.jsx
@@ -1,28 +1,50 @@
 import styles from "@/styles/pages/projects/Tekken7Homepage.module.scss";
+import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import tekken7Wallpaper from "@/public/assets/images/tekken7/tekken7_wallpaper.jpg";
 
 const Tekken7Homepage = () => {
-  const [isShow, setIsShow] = useState(false);
-  const handleClick = () => {
-    setIsShow(!isShow);
-  };
-
   return (
     <div className={styles.container}>
-      <div
+      <Image
+        src={tekken7Wallpaper}
+        alt={""}
+        fill
         style={{
-          height: "20px",
-          width: "20px",
-          background: "red",
-          opacity: isShow ? 1 : 0,
-          transition: "opacity 0s ease 2s",
+          objectFit: "cover",
+          zIndex: -1,
         }}
-      ></div>
-      <h1>Tekken 7</h1>
-      <h2>Stats and Combos</h2>
-      <button onClick={handleClick}>Sign up</button>
-      <Link href={"/projects/tekken7/login"}>Login</Link>
+      />
+
+      <div className={styles.loginContainer}>
+        <div className={styles.vStack}>
+          <h1 className={styles.heading}>Tekken 7</h1>
+          <h2 className={styles.subHeading}>Stats and Combos</h2>
+        </div>
+
+        <div className={`${styles.vStack}  ${styles.ctaButtonContainer}`}>
+          <form className={styles.form}>
+            <div className={styles.inputContainer}>
+              <label className={styles.inputLabel}>Username</label>
+              <input type={"text"} />
+            </div>
+
+            <div className={styles.inputContainer}>
+              <label className={styles.inputLabel}>Password</label>
+              <input type={"password"} />
+            </div>
+          </form>
+
+          <button className={styles.signUpButton}>SIGN UP</button>
+          <Link
+            href={"/projects/tekken7/login"}
+            className={styles.loginButton}
+          >
+            Already a member? Login
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
